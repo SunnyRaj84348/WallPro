568f7bf9e097ef1d7aa5d57f63cb512ca51662bc
ujjwalk9128 <ujjwalkumar0908@gmail.com>
2024-08-15 19:25:46 +0530

    first commit

---
+++ wallpro.js
@@ -0,0 +1,18 @@
+// Function to handle the download
+function downloadImage(imageSrc) {
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.href = imageSrc;
+    link.download = imageSrc.split('/').pop(); // Set the download attribute with the file name
+
+    // Trigger the download
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+// Function to handle button click event
+function handleDownloadClick(event, imageSrc) {
+    event.preventDefault(); // Prevent default button action
+    downloadImage(imageSrc);
+}
